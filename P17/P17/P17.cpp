469a302a3e8f69b4433811e6e33151fc16c0e190
sky960802-source <sky960802@gmail.com>
2025-12-25 02:27:56 +0800

    Add files via upload

---
+++ P17/P17/P17.cpp
@@ -0,0 +1,22 @@
+﻿/*使用fread()函數讀取檔案內容*/
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <stdlib.h>
+#define MAX 80
+
+int main(void)
+{
+    FILE* fptr;
+    char str[MAX];
+    int bytes;          //存放fread()成功讀取的字元數
+    fptr = fopen("output.txt", "r");
+    while (!feof(fptr)) //如果還沒讀到檔尾
+    {
+        bytes = fread(str, sizeof(char), MAX - 1, fptr);
+        str[bytes] = '\0';
+        printf("%s\n", str); //印出檔案內容
+    }
+    fclose(fptr);       //關閉檔案
+    system("pause");
+    return 0;
+}
