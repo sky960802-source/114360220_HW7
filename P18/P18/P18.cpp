3602b3776749548e5e418c6003fc618c2f6164fe
sky960802-source <sky960802@gmail.com>
2025-12-25 02:31:27 +0800

    Add files via upload

---
+++ P18/P18/P18.cpp
@@ -0,0 +1,22 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+#include<stdio.h>
+#include<stdlib.h>
+
+int main(void)
+{
+    FILE* in, * out;
+    int cnt;
+    char str[100];
+    in = fopen("welcome.txt", "r");
+    out = fopen("output.txt", "w");
+    while (!feof(in))
+    {
+        cnt = fscanf(in, "%s", str);//讀取檔案
+        if (cnt > 0)
+            fprintf(out, "%s\n", str);//寫入檔案
+    }
+    fclose(in);       //關閉檔案
+    fclose(out);      //關閉檔案
+    system("pause");
+    return 0;
+}
